b473bd160559341ac0e1c44ea6febc1a5183c5fb
Stuart Moncrieff <stuart@myloadtest.com>
2013-08-11 12:40:21 +1000

    Function documentation
    Documentation in Doxygen format. Also changed some lr_error_message and
    lr_output_message strings.

--- IDocReplayDLL/IDocReplayDLL.cpp
+++ IDocReplayDLL/IDocReplayDLL.cpp
@@ -16,7 +16,7 @@ using namespace pugi;
 using namespace std;
 
 bool g_licenseValid = false;  // Global variable. Note that this variable will retain its value for the lifetime of the
-// DLL (it does not need to be declared "static").
+                              // DLL (it does not need to be declared "static").
 
 // The input file contains XML values that can be extracted by idoc_eval_string().
 std::string g_idocParamInputFilePath;
@@ -24,36 +24,48 @@ pugi::xml_document g_idocParamInputFile;
 
 std::vector<std::string> g_allocatedStrings;
 
+// IDoc parameters have the format: {IDoc:SegmentName:FieldName}
+// Parameter names must contain only alphanumeric characters and underscores.
 static const regex g_parameterRegex("\\{idoc\\:([^:}]*)\\:([^:}]*)\\}", regex_constants::icase);
 static const regex g_parameterNameRegex("^[a-zA-Z_][a-zA-Z_0-9]*$");
 
+// This section contains functions that are used internally by the DLL.
+// These functions cannot be called from a VuGen script.
 namespace
 {
-    // Returns true if the "Parameter substitution" checkbox is selected in the VuGen script Runtime Settings.
-    // Use it like this:
-    //      if (is_param_log_enabled()) { lr_output_message("my message about params"); }
+    /// @brief Determines if "parameter substitution" logging is enabled
+	/// @details Runtime Settings > General > Log: Is the "parameter substitution" checkbox ticked?
+	///     Note that logging levels may be changed at runtime.
+	/// @return true if the "parameter substitution" checkbox is ticked.
+	/// @example
+    ///     if (is_param_log_enabled()) { lr_output_message("my message about params"); }
     bool is_param_log_enabled()
     {
         unsigned int log_options = lr_get_debug_message();
         return (log_options & LR_MSG_CLASS_PARAMETERS) != 0;
     }
 
-    // Returns true if the "Advanced trace" checkbox is selected in the VuGen script Runtime Settings.
-    // Use it like this:
-    //      if (is_trace_log_enabled()) { lr_output_message("my message about trace"); }
+    /// @brief Determines if "advanced trace" logging is enabled
+	/// @details Runtime Settings > General > Log: Is the "advanced trace" checkbox ticked?
+	///     Note that logging levels may be changed at runtime.
+	/// @return true if the "advanced trace" checkbox is ticked.
+    /// @example
+    ///     if (is_trace_log_enabled()) { lr_output_message("my message about trace"); }
     bool is_trace_log_enabled()
     {
         unsigned int log_options = lr_get_debug_message();
         return (log_options & LR_MSG_CLASS_FULL_TRACE) != 0;
     }
 
+	/// @brief Determines if the current license is valid
+	/// @details Before any functions in idoc.dll are called, a license key must be set using the
+	///     idoc_set_license function.
+	/// @return true if the current license key is valid
     bool ensure_valid_license()
     {
         if (!g_licenseValid)
         {
-            lr_error_message(
-                "[%s] ERROR: Valid license is not specified. Call idoc_set_license() first.",
-                __FUNCTION__);
+            lr_error_message("Valid license is not specified. Call idoc_set_license() first.");
             return false;
         }
 
@@ -99,14 +111,28 @@ namespace
     }
 }
 
-// TODO (will do this last, as it is non-essential functionality.
+/// @brief Sets the license key to use for idoc.dll 
+/// @details The idoc_create function will not work unless the license key has been set and is 
+///     valid. If the license key is invalid, an error message will be written to the Replay log.
+/// @param a string containing a license in XML format
+/// @example
+///     idoc_set_license(
+///         "<license>"
+///         "    <name>Joe User</name>"
+///         "    <company>BigCo</company>"
+///         "    <email>joe.user@example.com</email>"
+///         "    <key>ANUAA-ADHHB-BS7VU-MVH45-9ZG3B-U3PUQ</key>"
+///         "    <expires>2013-10-01</expires>"
+///         "</license>");
+/// @todo Add the license key functionality using the SoftActivate API. Note: I will do this last,
+///     as it is non-essential functionality.
 IDOCREPLAYDLL_API BOOL idoc_set_license(const LPCSTR licenseXml)
 {
     g_licenseValid = false;
 
     if (licenseXml == NULL)
     {
-        lr_error_message("[%s] License cannot be NULL.", __FUNCTION__);
+        lr_error_message("[%s] License key cannot be NULL.", __FUNCTION__);
         return g_licenseValid;
     }
 
@@ -139,7 +165,7 @@ IDOCREPLAYDLL_API BOOL idoc_set_license(const LPCSTR licenseXml)
     // TODO: add trace messages for "expired license" and "invalid license" (i.e. they have tampered with the fields)
     if (g_licenseValid && is_trace_log_enabled())
     {
-        lr_output_message("[%s] License key is valid. Expiry date: %TODO", __FUNCTION__);
+        lr_output_message("License key is valid. Expiry date: %TODO");
     }
 
     // The license key functionality has been implemented using the SoftActivate SDK. Example code for C++ can be
@@ -152,6 +178,9 @@ IDOCREPLAYDLL_API BOOL idoc_set_license(const LPCSTR licenseXml)
     return g_licenseValid;
 }
 
+/// @brief Select the input file from the Repository
+/// @param the file name (including path) of the IDoc to use as input for any IDoc parameters 
+/// @return true if the file exists and was selected successfully
 IDOCREPLAYDLL_API BOOL idoc_select_input_file(const LPCSTR filePath)
 {
     g_idocParamInputFilePath.erase();
@@ -187,6 +216,11 @@ IDOCREPLAYDLL_API BOOL idoc_select_input_file(const LPCSTR filePath)
     return TRUE;
 }
 
+/// @brief Replaces any IDoc parameters with a the specified value from the input IDoc. IDoc 
+///     parameters should be in the format {IDoc:SegmentName:FieldName}
+///     Note you must specify the input IDoc to use by first calling idoc_select_input_file
+/// @param a new string with the IDoc parameters replaced with their values
+/// @return a new string with the IDoc parameters replaced
 IDOCREPLAYDLL_API LPCSTR idoc_eval_string(const LPCSTR parameterizedString)
 {
     if (!ensure_valid_license())
@@ -196,7 +230,7 @@ IDOCREPLAYDLL_API LPCSTR idoc_eval_string(const LPCSTR parameterizedString)
 
     if (parameterizedString == NULL)
     {
-        lr_error_message("[%s] ERROR: Parameterized string cannot be NULL.", __FUNCTION__);
+        lr_error_message("Parameterized string cannot be empty.");
         return parameterizedString;
     }
 
@@ -204,7 +238,7 @@ IDOCREPLAYDLL_API LPCSTR idoc_eval_string(const LPCSTR parameterizedString)
     // We don't want an error to be raised when idoc_create() is called with standard LoadRunner parameters.
     if (g_idocParamInputFilePath.empty())
     {
-    //    lr_error_message("[%s] ERROR: Input file is not selected. (Call idoc_select_input_file first.)", __FUNCTION__);
+    //    lr_error_message("Input file is not selected. (Call idoc_select_input_file first.)");
         return parameterizedString;
     }
 
@@ -270,11 +304,15 @@ IDOCREPLAYDLL_API LPCSTR idoc_eval_string(const LPCSTR parameterizedString)
     return result.c_str();
 }
 
+/// TODO
+/// Note you must specify the input IDoc to use by first calling idoc_select_input_file
+/// @param TODO
+/// @return the number of occurrances of the specified element in the input IDoc
 IDOCREPLAYDLL_API int idoc_count_element(const LPCSTR elementName)
 {
     if (elementName == NULL)
     {
-        lr_error_message("[%s] ERROR: Element name cannot be NULL.", __FUNCTION__);
+        lr_error_message("[%s]: Element name cannot be NULL.", __FUNCTION__);
         return 0;
     }
 
@@ -285,7 +323,7 @@ IDOCREPLAYDLL_API int idoc_count_element(const LPCSTR elementName)
 
     if (g_idocParamInputFilePath.empty())
     {
-        lr_error_message("[%s] ERROR: Input file is not selected. (Call idoc_select_input_file first.)", __FUNCTION__);
+        lr_error_message("[%s]: Input file is not selected. (Call idoc_select_input_file first.)", __FUNCTION__);
         return 0;
     }
 
@@ -293,11 +331,14 @@ IDOCREPLAYDLL_API int idoc_count_element(const LPCSTR elementName)
     return result;
 }
 
+/// @brief Creates an IDoc file based on an XML template and returns it as a string
+/// @param a string containing the IDoc's XML template
+/// @return a string containing a new flat-text IDoc
 IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
 {
     if (idocXml == NULL)
     {
-        lr_error_message("[%s] ERROR: IDoc XML cannot be NULL.", __FUNCTION__);
+        lr_error_message("[%s]: IDoc XML cannot be NULL.", __FUNCTION__);
         return FALSE;
     }
 
@@ -314,14 +355,14 @@ IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
     doc.load(idocXmlFinal);
     if (doc.empty())
     {
-        lr_error_message("[%s] ERROR: The specified IDoc XML document is empty.", __FUNCTION__);
+        lr_error_message("[%s]: The specified IDoc XML document is empty.", __FUNCTION__);
         return NULL;
     }
 
     const xpath_node_set segmentNodeSet = doc.root().select_nodes("//IDOC/*[@SEGMENT='1']");
     if (segmentNodeSet.empty())
     {
-        lr_error_message("[%s] ERROR: The specified IDoc XML is not a valid IDoc.", __FUNCTION__);
+        lr_error_message("[%s]: The specified IDoc XML is not a valid IDoc.", __FUNCTION__);
         return NULL;
     }
 
@@ -336,7 +377,7 @@ IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
         if (fieldNodeSet.empty())
         {
             lr_error_message(
-                "[%s] ERROR: The specified IDoc XML contains segment '%s' without fields.",
+                "[%s]: The specified IDoc XML contains segment '%s' without fields.",
                 __FUNCTION__,
                 segmentNode.name());
             return NULL;
@@ -353,7 +394,7 @@ IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
             if (length == InvalidLength)
             {
                 lr_error_message(
-                    "[%s] ERROR: The specified IDoc XML contains field '%s:%s' without length or with invalid one.",
+                    "[%s]: The specified IDoc XML contains field '%s:%s' without length or with invalid one.",
                     __FUNCTION__,
                     segmentNode.name(),
                     fieldNode.name());
@@ -364,7 +405,7 @@ IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
             if (fieldText.length() > length)
             {
                 lr_error_message(
-                    "[%s] ERROR: The specified IDoc XML contains field '%s:%s' which actual length (%u) is greater"
+                    "[%s]: The specified IDoc XML contains field '%s:%s' which actual length (%u) is greater"
                         " than declared (%u).",
                     __FUNCTION__,
                     segmentNode.name(),
@@ -392,11 +433,15 @@ IDOCREPLAYDLL_API LPCSTR idoc_create_direct(const LPCSTR idocXml)
     return resultingDocument.c_str();
 }
 
+/// @brief Creates an IDoc file based on an XML template and saves it to a {parameter}
+/// @param the name of the LoadRuner {parameter} to save the IDoc to
+/// @param a string containing the IDoc's XML template
+/// @return true if the IDoc was created successfully
 IDOCREPLAYDLL_API BOOL idoc_create(const LPCSTR parameterName, const LPCSTR idocXml)
 {
     if (parameterName == NULL)
     {
-        lr_error_message("[%s] ERROR: Parameter name cannot be NULL.", __FUNCTION__);
+        lr_error_message("[%s]: Parameter name cannot be NULL.", __FUNCTION__);
         return FALSE;
     }
 
@@ -404,7 +449,7 @@ IDOCREPLAYDLL_API BOOL idoc_create(const LPCSTR parameterName, const LPCSTR idoc
     const bool parameterValid = regex_match(parameterName, dummyMatch, g_parameterNameRegex);
     if (!parameterValid)
     {
-        lr_error_message("[%s] ERROR: Parameter name is invalid.", __FUNCTION__);
+        lr_error_message("[%s]: Parameter name is invalid.", __FUNCTION__);
         return FALSE;
     }
 
@@ -418,12 +463,13 @@ IDOCREPLAYDLL_API BOOL idoc_create(const LPCSTR parameterName, const LPCSTR idoc
     return TRUE;
 }
 
+/// @brief Frees the memory used by ??
 IDOCREPLAYDLL_API void idoc_free_memory()
 {
     g_allocatedStrings.clear();
 }
 
-/// Saves a string (containing an IDoc) to the file system.
+/// @brief Saves a string (containing an IDoc) to the file system.
 /// @param the file name (including path) to save the IDoc to.
 /// @param the string containing the IDoc.
 /// @return true if the file was saved successfully.
